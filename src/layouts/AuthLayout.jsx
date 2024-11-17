4797fbff95394420ff5e0142a090d2c97251c30a
MursalinDevOps <mursalin1768@gmail.com>
2024-11-17 10:47:25 +0600

    login page added

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const AuthLayout = () => {
     return (
-        <div>
-            Auth Layout
+        <div className="font-poppins bg-[#f3f3f3]">
+            <header className="w-11/12 mx-auto py-5">
+                <Navbar></Navbar>
+            </header>
             <Outlet></Outlet>
         </div>
     );
