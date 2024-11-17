5dcc2da16d2afb5edfbd112ac568f830234a6876
MursalinDevOps <mursalin1768@gmail.com>
2024-11-17 11:15:34 +0600

    register page added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const Register = () => {
     return (
         <div className="min-h-screen flex justify-center items-center ">
             <div className="card bg-base-100 w-full max-w-xl shrink-0 rounded-none p-10">
-        <h2 className="text-3xl font-semibold text-center">Login your account</h2>
+        <h2 className="text-3xl font-semibold text-center">Register your account</h2>
                 <form className="card-body">
+                <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="your name" className="input input-bordered rounded-none" required />
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" placeholder="photo-url" className="input input-bordered rounded-none" required />
+                    </div>
+
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
@@ -17,18 +31,15 @@ const Login = () => {
                             <span className="label-text">Password</span>
                         </label>
                         <input type="password" placeholder="password" className="input input-bordered rounded-none" required />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn btn-neutral rounded-none">Login</button>
+                        <button className="btn btn-neutral rounded-none">Register</button>
                     </div>
                 </form>
-                    <p className="text-center">Dont’t Have An Account ? <Link className="text-pink-800" to='/auth/register'>Register</Link> </p>
+                    <p className="text-center">Already Have An Account ? <Link className="text-green-800" to='/auth/register'>Login</Link> </p>
             </div>
         </div>
     );
 };
 
-export default Login;
+export default Register;
